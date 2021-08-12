3064ebfd58571334510466ab11d21dd0e38247a0
Yuriy <yuri.zalukovskiy@gmail.com>
2021-08-12 17:13:20 +0300

    Initial Commit

---
+++ EdgeDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EdgeDetection
+//
+//  Created by Юрий Залуковский on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
