1b9f5bf9e9cecaa3a8ef1ee74db8156fd95264b3
Yuriy <yuri.zalukovskiy@gmail.com>
2021-08-13 11:36:53 +0300

    Completed task

---
+++ EdgeDetection/ResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ResultViewController.swift
+//  EdgeDetection
+//
+//  Created by Юрий Залуковский on 12.08.2021.
+//
+
+import Foundation
---
+++ EdgeDetection/StartViewController.swift
@@ -0,0 +1,8 @@
+//
+//  StartViewController.swift
+//  EdgeDetection
+//
+//  Created by Юрий Залуковский on 12.08.2021.
+//
+
+import Foundation
