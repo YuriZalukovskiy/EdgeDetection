e216c5e9e7d6f60e2f491953e006d4a3fc571b9d
Yuriy <yuri.zalukovskiy@gmail.com>
2021-08-13 11:40:36 +0300

    Completed task

--- EdgeDetection/ResultViewController.swift
+++ EdgeDetection/ResultViewController.swift
@@ -5,4 +5,24 @@
 //  Created by Юрий Залуковский on 12.08.2021.
 //
 
-import Foundation
+import UIKit
+import GPUImage
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var upperRenderView: RenderView!
+    @IBOutlet weak var lowerRenderView: RenderView!
+    
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let filter = SobelEdgeDetection()
+        let pictureInput = PictureInput(image: image!)
+        
+        pictureInput --> filter --> upperRenderView
+        pictureInput --> filter --> lowerRenderView
+        pictureInput.processImage(synchronously: true)
+    }
+}
--- EdgeDetection/StartViewController.swift
+++ EdgeDetection/StartViewController.swift
@@ -5,4 +5,43 @@
 //  Created by Юрий Залуковский on 12.08.2021.
 //
 
-import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var applyFilterButton: UIButton!
+    
+    @IBAction func chooseImagePressed(_ sender: UIButton) {
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = .photoLibrary
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func applyFilterPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let resultVC = segue.destination as! ResultViewController
+            resultVC.image = imageView.image!
+        }
+    }
+}
+
+extension StartViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            applyFilterButton.isEnabled = true
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
